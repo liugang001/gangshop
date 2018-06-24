c6ecb9bd4b27d91bd852f79aab7d9040f33dfc3a
Your Name <you@example.com>
2018-06-24 14:18:32 +0800

    组件拆分

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router';
 //css样式
 import "../../static/css/base.css";
-import "../../static/css/checkout.css";
-import "../../static/css/nav-header.css";
-import "../../static/css/nav-bread.css";
-import "../../static/css/nav-footer.css";
 import "../../static/css/goods-list.css";
 
 
 //路由配置
-const Hello=resolve=>require(['@/components/HelloWorld'],resolve);
 const goodsList=resolve=>require(["@/components/goods/goodsList"],resolve);
 
 Vue.use(Router)
@@ -19,10 +14,10 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: Hello
+      redirect:"/home"
     },
     {
-      path:"/goods",
+      path:"/home",
       component:goodsList
     }
   ]
